82ffbb0dc9b87c9a04e2451432010bdd8c45b26d
Rohit Shah <shahrohit2223@gmail.com>
2026-07-25 15:46:24 +0545

    level 4

--- 37 Design Pattern in JS/Design Pattern Project/app.js
+++ 37 Design Pattern in JS/Design Pattern Project/app.js
@@ -1,11 +1,57 @@
+//strategy pattern
+const sortStrategies={
+    alphabetical(notes){
+        return [...notes].sort((a,b)=>{
+            return a.text.localeCompare(b.text);
+        })
+    },
+    newest(notes){
+        return [...notes].sort((a,b)=>{
+         return   b.id-a.id
+        })
+    },
+    oldest(notes){
+        return [...notes].sort((a,b)=>{
+           return a.id-b.id
+        })
+    }
+}
+
+let currentStrategy=sortStrategies.alphabetical;
+
+//factory pattern
+function createNote(text){
+    return {
+        id: Date.now(),
+        text:text,
+        completed:false,
+    }
+}
+
 const NotesModule=(function (){
     let notes=[]; //private
+    const observers=[];
 
-    //public method
-    function addNote(note){
-        notes.push(note);
+    function notify(){
+        observers.forEach(function (observer){
+            observer();
+        })
     }
 
+    function subscribe(observer){
+        observers.push(observer)
+    }
+
+    //public method
+    // function addNote(note){
+    //     notes.push(note);
+    // }
+
+    function addNote(text){
+        const note=createNote(text);
+        notes.push(note);
+        notify();
+    }
     //public method
     function getNotes(){
         return notes;
@@ -13,7 +59,7 @@ const NotesModule=(function (){
 
     //expose only these methods
     return {
-        addNote,getNotes
+        addNote,getNotes,subscribe
     }
 })();
 
@@ -28,12 +74,27 @@ function renderNotes(){
     noteList.innerHTML="";
 
     //get all notes from the module
-    const notes=NotesModule.getNotes();
+    const notes=currentStrategy(
+        NotesModule.getNotes()
+    );
 
     //display each note
     notes.forEach(function (note){
         const li=document.createElement("li");
-        li.textContent=note;
+        li.textContent=note.text;
         noteList.appendChild(li)
     })
 }
+
+NotesModule.subscribe(renderNotes)
+
+addBtn.addEventListener("click",function(){
+    const note=noteInput.value.trim();
+
+    if(note==="") return;
+
+    NotesModule.addNote(note);
+    
+
+    noteInput.value="";
+})
